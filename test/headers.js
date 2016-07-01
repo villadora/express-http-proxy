281f1c83e06619d85d78fd1a9ba29c4b613cde27
monkpow <nkrimm@gmail.com>
2016-07-01 17:04:48 -0500

    Splitting the rest of the tests out, so we can have several small test files, rather than one large test file. (#88)

---
+++ test/headers.js
@@ -0,0 +1,45 @@
+var assert = require('assert');
+var express = require('express');
+var request = require('supertest');
+var fs = require('fs');
+var os = require('os');
+var proxy = require('../');
+
+describe('proxies headers', function() {
+  this.timeout(10000);
+
+  var other, http;
+
+  beforeEach(function () {
+    http = express();
+    http.use(proxy('http://httpbin.org', {
+      'headers': {
+          'X-Current-president': 'taft'
+      }
+    }));
+  });
+
+  it('passed as options', function (done) {
+    request(http)
+      .get('/headers')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        assert(res.body.headers['X-Current-President'] === 'taft');
+        done();
+      });
+  });
+
+  it('passed as on request', function (done) {
+    request(http)
+      .get('/headers')
+      .set('X-Powerererer', 'XTYORG')
+      .expect(200)
+      .end(function(err, res) {
+        if (err) return done(err);
+        assert(res.body.headers['X-Powerererer']);
+        done();
+      });
+  });
+
+});
---
+++ test/middlewareCompatibility.js
@@ -0,0 +1,43 @@
+var assert = require('assert');
+var express = require('express');
+var request = require('supertest');
+var proxy = require('../');
+
+describe('middleware compatibility', function() {
+  it('should use req.body if defined', function(done) {
+    var app = express();
+
+    // Simulate another middleware that puts req stream into the body
+    app.use(function(req, res, next) {
+      var received = [];
+      req.on('data', function onData(chunk) {
+        if (!chunk) return;
+        received.push(chunk);
+      });
+      req.on('end', function onEnd(err) {
+        received = Buffer.concat(received).toString('utf8');
+        req.body = JSON.parse(received);
+        req.body.foo = 1;
+        next();
+      });
+    });
+
+    app.use(proxy('example.com', {
+      intercept: function(rsp, data, req, res, cb) {
+        assert(req.body);
+        assert.equal(req.body.foo, 1);
+        assert.equal(req.body.mypost, 'hello');
+        cb(null, data);
+      }
+    }));
+
+    request(app)
+      .post('/post')
+      .send({
+        mypost: 'hello'
+      })
+      .end(function(err, res) {
+        done(err);
+      });
+  });
+});
