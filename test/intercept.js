8ff0afc012a5a331e53d705df25230d713dd43a8
Nik Krimm <nkrimm@gmail.com>
2016-07-01 13:40:08 -0500

    Clean up test names and re-order.  Drop github test outline, which I believe was incomplete and non-functional.

--- test/intercept.js
+++ test/intercept.js
@@ -4,7 +4,22 @@ var request = require('supertest');
 var proxy = require('../');
 
 describe('intercept', function () {
-  it('intercept', function (done) {
+  it('has access to original response', function (done) {
+    var app = express();
+    app.use(proxy('httpbin.org', {
+      intercept: function (rsp, data, req, res, cb) {
+        assert(rsp.connection);
+        assert(rsp.socket);
+        assert(rsp.headers);
+        assert(rsp.headers['content-type']);
+        done();
+      }
+    }));
+
+    request(app).get('/').end();
+  });
+
+  it('can modify the response data', function (done) {
     var app = express();
     app.use(proxy('httpbin.org', {
       intercept: function (rsp, data, req, res, cb) {
@@ -23,22 +38,8 @@ describe('intercept', function () {
     });
   });
 
-  it('test intercept original response', function (done) {
-    var app = express();
-    app.use(proxy('httpbin.org', {
-      intercept: function (rsp, data, req, res, cb) {
-        assert(rsp.connection);
-        assert(rsp.socket);
-        assert(rsp.headers);
-        assert(rsp.headers['content-type']);
-        done();
-      }
-    }));
 
-    request(app).get('/').end();
-  });
-
-  it('test intercept on html response', function (done) {
+  it('can mutuate an html response', function (done) {
     var app = express();
     app.use(proxy('httpbin.org', {
       intercept: function (rsp, data, req, res, cb) {
@@ -57,68 +58,42 @@ describe('intercept', function () {
     });
   });
 
-  describe('intercepting a redirect', function () {
-    it('allows you to update the location of the redirect', function (done) {
+  it('can change the location of a redirect', function (done) {
 
-      function redirectingServer(port, origin) {
-        var app = express();
-        app.get('/', function (req, res) {
-          res.status(302);
-          res.location(origin + '/proxied/redirect/url');
-          res.send();
-        });
-        return app.listen(port);
-      }
-
-      var redirectingServerPort = 8012;
-      var redirectingServerOrigin = ['http://localhost', redirectingServerPort].join(':');
-
-      var server = redirectingServer(redirectingServerPort, redirectingServerOrigin);
-
-      var proxyApp = express();
-      var preferredPort = 3000;
-
-      proxyApp.use(proxy(redirectingServerOrigin, {
-        intercept: function (rsp, data, req, res, cb) {
-          var proxyReturnedLocation = res._headers.location;
-          res.location(proxyReturnedLocation.replace(redirectingServerPort, preferredPort));
-          cb(null, data);
-        }
-      }));
-
-      request(proxyApp)
-      .get('/')
-      .expect(function (res) {
-        res.headers.location.match(/localhost:3000/);
-      })
-      .end(function () {
-        server.close();
-        done();
+    function redirectingServer(port, origin) {
+      var app = express();
+      app.get('/', function (req, res) {
+        res.status(302);
+        res.location(origin + '/proxied/redirect/url');
+        res.send();
       });
-    });
-  });
+      return app.listen(port);
+    }
 
-  it('test github api', function (done) {
-    var app = express();
-    app.use(proxy('https://api.github.com', {
+    var redirectingServerPort = 8012;
+    var redirectingServerOrigin = ['http://localhost', redirectingServerPort].join(':');
+
+    var server = redirectingServer(redirectingServerPort, redirectingServerOrigin);
+
+    var proxyApp = express();
+    var preferredPort = 3000;
+
+    proxyApp.use(proxy(redirectingServerOrigin, {
       intercept: function (rsp, data, req, res, cb) {
-        var Iconv = require('iconv').Iconv;
-        var iconv = new Iconv('UTF-8', 'utf8');
+        var proxyReturnedLocation = res._headers.location;
+        res.location(proxyReturnedLocation.replace(redirectingServerPort, preferredPort));
         cb(null, data);
       }
     }));
 
-    request(app)
+    request(proxyApp)
     .get('/')
-    .end(function (err, res) {
-      if (err) return done(err);
+    .expect(function (res) {
+      res.headers.location.match(/localhost:3000/);
+    })
+    .end(function () {
+      server.close();
       done();
     });
-
   });
 });
-
-//intercept: function (rsp, data, req, res, callback) {
-//res.set('Access-Control-Allow-Origin', 'localhost');
-//callback(null, data.toString('utf8'));
-//}
