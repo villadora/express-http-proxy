2655934aabd2725070cb22c4c07f30ae6bcca58a
Tor Hovland <teh@powel.com>
2016-07-04 20:32:57 +0100

    Introduced options.forwardPathAsync, to support scenarios where the determination of forward path needs to be asynchronous code.

--- test/forwardPath.js
+++ test/forwardPath.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var express = require('express');
 var request = require('supertest');
 var proxy = require('../');
+var Promise = require('es6-promise').Promise;
 
 describe('forwardPath', function() {
   'use strict';
@@ -29,6 +30,67 @@ describe('forwardPath', function() {
         return '/post';
       }
     }));
+
+    request(app)
+      .post('/foobar')
+      .send({
+        mypost: 'hello'
+      })
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        done(err);
+      });
+  });
+
+  it('test forwardPath to undefined yields 404', function(done) {
+    var app = express();
+    app.use(proxy('httpbin.org', {
+      forwardPath: undefined
+    }));
+
+    request(app)
+      .post('/foobar')
+      .send({
+        mypost: 'hello'
+      })
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 404);
+        done(err);
+      });
+  });
+
+  it('test forwardPath as an async function should not work', function(done) {
+    var app = express();
+    app.use(proxy('httpbin.org', {
+      forwardPath: function() {
+        setTimeout(function () {
+          return '/post';        
+        }, 100);
+      }
+    }));
+
+    request(app)
+      .post('/foobar')
+      .send({
+        mypost: 'hello'
+      })
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 405);
+        done(err);
+      });
+  });
+
+  it('test forwardPathAsync to known path yields 200', function(done) {
+    var app = express();
+    app.use(proxy('httpbin.org', {
+      forwardPathAsync: function() {
+        return new Promise(function(resolve) {
+          setTimeout(function () {
+            resolve('/post');        
+          }, 250);
+        });
+      }}
+    ));
 
     request(app)
       .post('/foobar')
