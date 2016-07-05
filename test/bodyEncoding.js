f211cd31268ce41d8bedf53a876e63c5757e8617
Nik Krimm <nkrimm@gmail.com>
2016-07-05 08:16:10 -0500

    Adding jscs and linting current files.

--- test/bodyEncoding.js
+++ test/bodyEncoding.js
@@ -18,25 +18,34 @@ describe('body encoding', function() {
 
 
   it('allow raw data', function(done) {
-    var png_data = new Buffer('89504e470d0a1a0a0000000d49484452000000010000000108060000001f15c4890000000a49444154789c63000100000500010d0a2db40000000049454e44ae426082', 'hex');
-    var filename = os.tmpdir() + '/express-http-proxy-test-' + Math.floor((Math.random() * 1000) + 1) + '-png-transparent.png';
+    var pngHex = '89504e470d0a1a0a0' +
+                 '000000d4948445200' +
+                 '00000100000001080' +
+                 '60000001f15c48900' +
+                 '00000a49444154789' +
+                 'c6300010000050001' +
+                 '0d0a2db4000000004' +
+                 '9454e44ae426082';
+    var pngData = new Buffer(pngHex, 'hex');
+    var filename = os.tmpdir() + '/express-http-proxy-test-' + (new Date()).getTime() + '-png-transparent.png';
     var app = express();
     app.use(proxy('httpbin.org', {
       reqBodyEncoding: null,
       decorateRequest: function(reqOpts) {
-        assert((new Buffer(reqOpts.bodyContent).toString('hex')).indexOf(png_data.toString('hex')) >= 0, 'body should contain same data');
+        assert((new Buffer(reqOpts.bodyContent).toString('hex')).indexOf(pngData.toString('hex')) >= 0,
+          'body should contain same data');
         return reqOpts;
       }
     }));
 
-    fs.writeFile(filename, png_data, function(err) {
+    fs.writeFile(filename, pngData, function(err) {
       if (err) { throw err; }
       request(app)
         .post('/post')
         .attach('image', filename)
         .end(function(err, res) {
           fs.unlink(filename);
-          assert.equal(res.body.files.image, 'data:image/png;base64,' + png_data.toString('base64'));
+          assert.equal(res.body.files.image, 'data:image/png;base64,' + pngData.toString('base64'));
           done(err);
         });
     });
