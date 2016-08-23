villadora/express-http-proxy

commit 7d6e4f8384ed1de438de92dc92b636c861e934c2

Forward path async.rebased (#110)
* Introduced options.forwardPathAsync, to support scenarios where the determination of forward path needs to be asynchronous code.

* A few minor tweaks to forwardPathAsync code.

* Add a test:debug task.

index.js
@@ -3,6 +3,7 @@ var url = require('url');
 var http = require('http');
 var https = require('https');
 var getRawBody = require('raw-body');
+var promise = require('es6-promise');
 
 require('buffer');
 
@@ -18,19 +19,24 @@ module.exports = function proxy(host, options) {
   /**
    * Function :: intercept(targetResponse, data, res, req, function(err, json));
    */
-  var intercept = options.intercept;   // after request (5)
-  var decorateRequest = options.decorateRequest;  // before request (2)
-  var forwardPath = options.forwardPath || defaultForwardPath;  // before request (1)
-  var filter = options.filter || defaultFilter;  // before request (0)
-  var limit = options.limit || '1mb';  // option
-  var preserveReqSession = options.preserveReqSession; // binary option
+  var intercept = options.intercept;
+  var decorateRequest = options.decorateRequest;
+  var forwardPath = options.forwardPath || defaultForwardPath;
+  var forwardPathAsync = options.forwardPathAsync || defaultForwardPathAsync(forwardPath);
+  var filter = options.filter || defaultFilter;
+  var limit = options.limit || '1mb';
+  var preserveReqSession = options.preserveReqSession;
 
   return function handleProxy(req, res, next) {
-
     if (!filter(req, res)) { return next(); }
 
-    var path = forwardPath(req, res);
+    forwardPathAsync(req, res)
+      .then(function(path) {
+        proxyWithResolvedPath(req, res, next, path);
+      });
+  };
 
+  function proxyWithResolvedPath(req, res, next, path) {
     parsedHost = parsedHost || parseHost(host, req);
 
     if (req.body) {
@@ -166,7 +172,7 @@ module.exports = function proxy(host, options) {
         realRequest.abort();
       });
     }
-  };
+  }
 };
 
 
@@ -252,10 +258,20 @@ function bodyEncoding(options) {
   return options.reqBodyEncoding !== undefined ? options.reqBodyEncoding: 'utf-8';
 }
 
+
 function length(chunks) {
   'use strict';
 
   return chunks.reduce(function(len, buf) {
     return len + buf.length;
   }, 0);
+}
+
+function defaultForwardPathAsync(forwardPath) {
+  'use strict';
+  return function(req) {
+    return new promise.Promise(function(resolve) {
+      resolve(forwardPath(req));
+    });
+  };
 }

test/forwardPath.js
@@ -2,6 +2,7 @@ var assert = require('assert');
 var express = require('express');
 var request = require('supertest');
 var proxy = require('../');
+var promise = require('es6-promise');
 
 describe('forwardPath', function() {
   'use strict';
@@ -27,6 +28,86 @@ describe('forwardPath', function() {
     app.use(proxy('httpbin.org', {
       forwardPath: function() {
         return '/post';
+      }
+    }));
+
+    request(app)
+      .post('/foobar')
+      .send({
+        mypost: 'hello'
+      })
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        done(err);
+      });
+  });
+
+  it('test forwardPath to undefined yields 404', function(done) {
+    var app = express();
+    app.use(proxy('httpbin.org', {
+      forwardPath: undefined
+    }));
+
+    request(app)
+      .post('/foobar')
+      .send({
+        mypost: 'hello'
+      })
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 404);
+        done(err);
+      });
+  });
+
+  it('test forwardPath as an async function should not work', function(done) {
+    var app = express();
+    app.use(proxy('httpbin.org', {
+      forwardPath: function() {
+        setTimeout(function() {
+          return '/post';
+        }, 100);
+      }
+    }));
+
+    request(app)
+      .post('/foobar')
+      .send({
+        mypost: 'hello'
+      })
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 405);
+        done(err);
+      });
+  });
+
+  it('test forwardPathAsync to known path yields 200', function(done) {
+    var app = express();
+    app.use(proxy('httpbin.org', {
+      forwardPathAsync: function() {
+        return new promise.Promise(function(resolve) {
+          setTimeout(function() {
+            resolve('/post');
+          }, 250);
+        });
+      }}
+    ));
+
+    request(app)
+      .post('/foobar')
+      .send({
+        mypost: 'hello'
+      })
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        done(err);
+      });
+  });
+
+  it('test forwardPathAsync to known path (as function) yields 200', function(done) {
+    var app = express();
+    app.use(proxy('httpbin.org', {
+      forwardPath: function() {
+        return ('/post');
       }
     }));
 

commit 912037e0b050d646bcb9657e858b8397e87efd02

Add response when calling `forwardPath`.
This is part of the API and was broken with 7d6e4f8 see https://github.com/simono/express-http-proxy/commit/7d6e4f8384ed1de438de92dc92b636c861e934c2#diff-168726dbe96b3ce427e7fedce31bb0bcL32

index.js
@@ -271,9 +271,9 @@ function chunkLength(chunks) {
 
 function defaultForwardPathAsync(forwardPath) {
   'use strict';
-  return function(req) {
+  return function(req, res) {
     return new promise.Promise(function(resolve) {
-      resolve(forwardPath(req));
+      resolve(forwardPath(req, res));
     });
   };
 }

test/forwardPath.js
@@ -42,6 +42,27 @@ describe('forwardPath', function() {
       });
   });
 
+  it('test forwardPath to known path with request and response yields 200', function(done) {
+    var app = express();
+    app.use(proxy('httpbin.org', {
+      forwardPath: function(req, res) {
+        assert.ok(req);
+        assert.ok(res);
+        return '/post';
+      }
+    }));
+
+    request(app)
+      .post('/foobar')
+      .send({
+        mypost: 'hello'
+      })
+      .end(function(err, res) {
+        assert.equal(res.statusCode, 200);
+        done(err);
+      });
+  });
+
   it('test forwardPath to undefined yields 404', function(done) {
     var app = express();
     app.use(proxy('httpbin.org', {