a16ff7242006c07ad43f55d8958c71d0109f9a4b
Nik Krimm <nkrimm@gmail.com>
2016-08-30 16:44:46 -0500

    [adhoc] Add tests for host function.

---
+++ test/host.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var request = require('supertest');
+var proxy = require('../');
+
+describe('host can be a dynamic function', function() {
+  'use strict';
+
+  this.timeout(10000);
+
+  var app = express();
+  var proxyApp = express();
+  var randomPort = Math.floor(Math.random() * 10000);
+  var proxyUrl = "localhost:" + randomPort;
+
+  app.use(function (req, res, next) {
+    req.session = { dynamic_host: proxyUrl };
+    next();
+  });
+
+  app.use('/proxy', proxy(function (req) {
+    return req.session.dynamic_host;
+  }));
+
+  proxyApp.get('/', function (req, res) {
+    res.sendStatus(204);
+  });
+  proxyApp.listen(randomPort);
+
+  it('can proxy with session value', function(done) {
+    request(app)
+      .get('/proxy')
+      .expect(204, done);
+  });
+});
