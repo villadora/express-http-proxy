6205e0690719c173a9c3d0732718a05aa43be828
Nik Krimm <nkrimm@gmail.com>
2017-02-16 11:54:33 -0600

    linting

--- test/bodyEncoding.js
+++ test/bodyEncoding.js
@@ -1,15 +1,15 @@
-var assert = require('assert');
-var express = require('express');
-var request = require('supertest');
-var fs = require('fs');
-var os = require('os');
-var proxy = require('../');
+var assert = require('assert')
+var express = require('express')
+var request = require('supertest')
+var fs = require('fs')
+var os = require('os')
+var proxy = require('../')
 
-describe('body encoding', function() {
-  'use strict';
-  this.timeout(10000);
+describe('body encoding', function () {
+  'use strict'
+  this.timeout(10000)
 
-  var app;
+  var app
   var pngHex = '89504e470d0a1a0a0' +
                '000000d4948445200' +
                '00000100000001080' +
@@ -17,103 +17,97 @@ describe('body encoding', function() {
                '00000a49444154789' +
                'c6300010000050001' +
                '0d0a2db4000000004' +
-               '9454e44ae426082';
-  var pngData = new Buffer(pngHex, 'hex');
+               '9454e44ae426082'
+  var pngData = new Buffer(pngHex, 'hex')
 
+  beforeEach(function () {
+    app = express()
+    app.use(proxy('httpbin.org'))
+  })
 
-  beforeEach(function() {
-    app = express();
-    app.use(proxy('httpbin.org'));
-  });
-
-
-  it('allow raw data', function(done) {
-    var filename = os.tmpdir() + '/express-http-proxy-test-' + (new Date()).getTime() + '-png-transparent.png';
-    var app = express();
+  it('allow raw data', function (done) {
+    var filename = os.tmpdir() + '/express-http-proxy-test-' + (new Date()).getTime() + '-png-transparent.png'
+    var app = express()
     app.use(proxy('httpbin.org', {
       reqBodyEncoding: null,
-      decorateRequest: function(reqOpts) {
+      decorateRequest: function (reqOpts) {
         assert((new Buffer(reqOpts.bodyContent).toString('hex')).indexOf(pngData.toString('hex')) >= 0,
-          'body should contain same data');
-        return reqOpts;
+          'body should contain same data')
+        return reqOpts
       }
-    }));
+    }))
 
-    fs.writeFile(filename, pngData, function(err) {
-      if (err) { throw err; }
+    fs.writeFile(filename, pngData, function (err) {
+      if (err) { throw err }
       request(app)
         .post('/post')
         .attach('image', filename)
-        .end(function(err, res) {
-          fs.unlink(filename);
-          assert.equal(res.body.files.image, 'data:image/png;base64,' + pngData.toString('base64'));
-          done(err);
-        });
-    });
+        .end(function (err, res) {
+          fs.unlink(filename)
+          assert.equal(res.body.files.image, 'data:image/png;base64,' + pngData.toString('base64'))
+          done(err)
+        })
+    })
+  })
 
-  });
-
-  describe('when user sets parseReqBody', function() {
-    it('should not parse body', function(done) {
-      var filename = os.tmpdir() + '/express-http-proxy-test-' + (new Date()).getTime() + '-png-transparent.png';
-      var app = express();
+  describe('when user sets parseReqBody', function () {
+    it('should not parse body', function (done) {
+      var filename = os.tmpdir() + '/express-http-proxy-test-' + (new Date()).getTime() + '-png-transparent.png'
+      var app = express()
       app.use(proxy('httpbin.org', {
         parseReqBody: false,
-        decorateRequest: function(reqOpts) {
-          assert(!reqOpts.bodyContent, 'body content should not be parsed.');
-          return reqOpts;
+        decorateRequest: function (reqOpts) {
+          assert(!reqOpts.bodyContent, 'body content should not be parsed.')
+          return reqOpts
         }
-      }));
+      }))
 
-      fs.writeFile(filename, pngData, function(err) {
-        if (err) { throw err; }
+      fs.writeFile(filename, pngData, function (err) {
+        if (err) { throw err }
         request(app)
           .post('/post')
           .attach('image', filename)
-          .end(function(err, res) {
-            fs.unlink(filename);
-            assert.equal(res.body.files.image, 'data:image/png;base64,' + pngData.toString('base64'));
-            done(err);
-          });
-      });
-    });
-    it('should not fail on large limit', function(done) {
-      var filename = os.tmpdir() + '/express-http-proxy-test-' + (new Date()).getTime() + '-png-transparent.png';
-      var app = express();
+          .end(function (err, res) {
+            fs.unlink(filename)
+            assert.equal(res.body.files.image, 'data:image/png;base64,' + pngData.toString('base64'))
+            done(err)
+          })
+      })
+    })
+    it('should not fail on large limit', function (done) {
+      var filename = os.tmpdir() + '/express-http-proxy-test-' + (new Date()).getTime() + '-png-transparent.png'
+      var app = express()
       app.use(proxy('httpbin.org', {
         parseReqBody: false,
-        limit: '20gb',
-      }));
-      fs.writeFile(filename, pngData, function(err) {
-        if (err) { throw err; }
+        limit: '20gb'
+      }))
+      fs.writeFile(filename, pngData, function (err) {
+        if (err) { throw err }
         request(app)
           .post('/post')
           .attach('image', filename)
-          .end(function(err, res) {
-            fs.unlink(filename);
-            assert.equal(res.body.files.image, 'data:image/png;base64,' + pngData.toString('base64'));
-            done(err);
-          });
-      });
-    });
-  });
+          .end(function (err, res) {
+            fs.unlink(filename)
+            assert.equal(res.body.files.image, 'data:image/png;base64,' + pngData.toString('base64'))
+            done(err)
+          })
+      })
+    })
+  })
 
-
-  describe('when user sets reqBodyEncoding', function() {
-    it('should set the accepts-charset header', function(done) {
-      var app = express();
+  describe('when user sets reqBodyEncoding', function () {
+    it('should set the accepts-charset header', function (done) {
+      var app = express()
       app.use(proxy('httpbin.org', {
         reqBodyEncoding: 'utf-16'
-      }));
+      }))
       request(app)
         .get('/headers')
-        .end(function(err, res) {
-          if (err) { throw err; }
-          assert.equal(res.body.headers['Accept-Charset'], 'utf-16');
-          done(err);
-        });
-    });
-  });
-
-
-});
+        .end(function (err, res) {
+          if (err) { throw err }
+          assert.equal(res.body.headers['Accept-Charset'], 'utf-16')
+          done(err)
+        })
+    })
+  })
+})
--- test/intercept.js
+++ test/intercept.js
@@ -1,127 +1,124 @@
-var assert = require('assert');
-var express = require('express');
-var request = require('supertest');
-var proxy = require('../');
+var assert = require('assert')
+var express = require('express')
+var request = require('supertest')
+var proxy = require('../')
 
-describe('intercept', function() {
-  'use strict';
+describe('intercept', function () {
+  'use strict'
 
-  it('has access to original response', function(done) {
-    var app = express();
+  it('has access to original response', function (done) {
+    var app = express()
     app.use(proxy('httpbin.org', {
-      intercept: function(rsp) {
-        assert(rsp.connection);
-        assert(rsp.socket);
-        assert(rsp.headers);
-        assert(rsp.headers['content-type']);
-        done();
+      intercept: function (rsp) {
+        assert(rsp.connection)
+        assert(rsp.socket)
+        assert(rsp.headers)
+        assert(rsp.headers['content-type'])
+        done()
       }
-    }));
+    }))
 
-    request(app).get('/').end();
-  });
+    request(app).get('/').end()
+  })
 
-  it('can modify the response data', function(done) {
-    var app = express();
+  it('can modify the response data', function (done) {
+    var app = express()
     app.use(proxy('httpbin.org', {
-      intercept: function(rsp, data, req, res, cb) {
-        data = JSON.parse(data.toString('utf8'));
-        data.intercepted = true;
-        cb(null, JSON.stringify(data));
+      intercept: function (rsp, data, req, res, cb) {
+        data = JSON.parse(data.toString('utf8'))
+        data.intercepted = true
+        cb(null, JSON.stringify(data))
       }
-    }));
+    }))
 
     request(app)
     .get('/ip')
-    .end(function(err, res) {
-      if (err) { return done(err); }
-      assert(res.body.intercepted);
-      done();
-    });
-  });
+    .end(function (err, res) {
+      if (err) { return done(err) }
+      assert(res.body.intercepted)
+      done()
+    })
+  })
 
-
-  it('can modify the response headers', function(done) {
-    var app = express();
+  it('can modify the response headers', function (done) {
+    var app = express()
     app.use(proxy('httpbin.org', {
-      intercept: function(rsp, data, req, res, cb) {
-        res.set('x-wombat-alliance', 'mammels');
-        res.set('content-type', 'wiki/wiki');
-        cb(null, data);
+      intercept: function (rsp, data, req, res, cb) {
+        res.set('x-wombat-alliance', 'mammels')
+        res.set('content-type', 'wiki/wiki')
+        cb(null, data)
       }
-    }));
+    }))
 
     request(app)
     .get('/ip')
-    .end(function(err, res) {
-      if (err) { return done(err); }
-      assert(res.headers['content-type'] === 'wiki/wiki');
-      assert(res.headers['x-wombat-alliance'] === 'mammels');
-      done();
-    });
-  });
+    .end(function (err, res) {
+      if (err) { return done(err) }
+      assert(res.headers['content-type'] === 'wiki/wiki')
+      assert(res.headers['x-wombat-alliance'] === 'mammels')
+      done()
+    })
+  })
 
-  it('can mutuate an html response', function(done) {
-    var app = express();
+  it('can mutuate an html response', function (done) {
+    var app = express()
     app.use(proxy('httpbin.org', {
-      intercept: function(rsp, data, req, res, cb) {
-        data = data.toString().replace('Oh', '<strong>Hey</strong>');
-        assert(data !== '');
-        cb(null, data);
+      intercept: function (rsp, data, req, res, cb) {
+        data = data.toString().replace('Oh', '<strong>Hey</strong>')
+        assert(data !== '')
+        cb(null, data)
       }
-    }));
+    }))
 
     request(app)
     .get('/html')
-    .end(function(err, res) {
-      if (err) { return done(err); }
-      assert(res.text.indexOf('<strong>Hey</strong>') > -1);
-      done();
-    });
-  });
+    .end(function (err, res) {
+      if (err) { return done(err) }
+      assert(res.text.indexOf('<strong>Hey</strong>') > -1)
+      done()
+    })
+  })
 
-  it('can change the location of a redirect', function(done) {
-
-    function redirectingServer(port, origin) {
-      var app = express();
-      app.get('/', function(req, res) {
-        res.status(302);
-        res.location(origin + '/proxied/redirect/url');
-        res.send();
-      });
-      return app.listen(port);
+  it('can change the location of a redirect', function (done) {
+    function redirectingServer (port, origin) {
+      var app = express()
+      app.get('/', function (req, res) {
+        res.status(302)
+        res.location(origin + '/proxied/redirect/url')
+        res.send()
+      })
+      return app.listen(port)
     }
 
-    var redirectingServerPort = 8012;
-    var redirectingServerOrigin = ['http://localhost', redirectingServerPort].join(':');
+    var redirectingServerPort = 8012
+    var redirectingServerOrigin = ['http://localhost', redirectingServerPort].join(':')
 
-    var server = redirectingServer(redirectingServerPort, redirectingServerOrigin);
+    var server = redirectingServer(redirectingServerPort, redirectingServerOrigin)
 
-    var proxyApp = express();
-    var preferredPort = 3000;
+    var proxyApp = express()
+    var preferredPort = 3000
 
     proxyApp.use(proxy(redirectingServerOrigin, {
-      intercept: function(rsp, data, req, res, cb) {
-        var proxyReturnedLocation = res._headers.location;
-        res.location(proxyReturnedLocation.replace(redirectingServerPort, preferredPort));
-        cb(null, data);
+      intercept: function (rsp, data, req, res, cb) {
+        var proxyReturnedLocation = res._headers.location
+        res.location(proxyReturnedLocation.replace(redirectingServerPort, preferredPort))
+        cb(null, data)
       }
-    }));
+    }))
 
     request(proxyApp)
     .get('/')
-    .expect(function(res) {
-      res.headers.location.match(/localhost:3000/);
+    .expect(function (res) {
+      res.headers.location.match(/localhost:3000/)
     })
-    .end(function() {
-      server.close();
-      done();
-    });
-  });
-});
+    .end(function () {
+      server.close()
+      done()
+    })
+  })
+})
 
-
-describe('test intercept on html response from github',function() {
+describe('test intercept on html response from github', function () {
   /*
      Github provided a unique situation where the encoding was different than
      utf-8 when we didn't explicitly ask for utf-8.  This test helped sort out
@@ -130,27 +127,26 @@ describe('test intercept on html response from github',function() {
      issue.
   */
 
-  'use strict';
+  'use strict'
 
-  it('is able to read and manipulate the response', function(done) {
-    this.timeout(1500);  // give it some extra time to get response
-    var app = express();
+  it('is able to read and manipulate the response', function (done) {
+    this.timeout(1500)  // give it some extra time to get response
+    var app = express()
     app.use(proxy('https://github.com/villadora/express-http-proxy', {
-      intercept: function(targetResponse, data, req, res, cb) {
-        data = data.toString().replace('DOCTYPE','WINNING');
-        assert(data !== '');
-        cb(null, data);
+      intercept: function (targetResponse, data, req, res, cb) {
+        data = data.toString().replace('DOCTYPE', 'WINNING')
+        assert(data !== '')
+        cb(null, data)
       }
-    }));
+    }))
 
     request(app)
     .get('/html')
-    .end(function(err, res) {
-      if (err) { return done(err); }
-      assert(res.text.indexOf('WINNING') > -1);
-      done();
-    });
-
-  });
-});
+    .end(function (err, res) {
+      if (err) { return done(err) }
+      assert(res.text.indexOf('WINNING') > -1)
+      done()
+    })
+  })
+})
 
