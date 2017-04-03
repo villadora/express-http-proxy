51b43e49431dcc175fd05febaee2c5e579296f0e
Nik Krimm <nkrimm@gmail.com>
2017-04-03 14:31:05 -0500

    Add missed files

---
+++ test/support/proxyTarget.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var express = require('express')
+var chunkLength = require('../../lib/chunkLength');
+
+function proxyTarget(port, timeout) {
+  var target = express();
+
+  timeout = 1000 || timeout;
+
+  target.get('/', function(req, res) {
+    setTimeout(function() {
+      res.send('Success');
+    },timeout);
+  });
+
+  target.post('/post', function (req, res, next) {
+    req.pipe(res);
+    //var chunks = [];
+    //req.on('data', function(chunk) { chunks.push(chunk); });
+    //req.on('end', function()       {
+      //var upload = Buffer.concat(chunks, chunkLength(chunks));
+      //debugger;
+      //res.write(upload);
+      //res.end();
+      //res.json({
+        //attachment: upload
+      //});
+    //});
+    //req.on('error', function(err)  { next(err); });
+  });
+
+  target.use(function (req, res, next) {
+    res.send('Failure');
+  });
+
+  return target.listen(port);
+}
+
+module.exports = proxyTarget;
