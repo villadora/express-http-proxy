5ec1f21677f6974805ab89ce49c739132b93ce0e
Nik Krimm <nkrimm@gmail.com>
2017-04-04 16:23:20 -0500

    Extract sendUserReq to module; other minor cleanup.

--- index.js
+++ index.js
@@ -4,22 +4,22 @@
 // There are a lot of competing strategies in this code.
 // It would be easier to follow if we extract to simpler functions, and used
 // a standard, step-wise set of filters with clearer edges and borders.
-// Currently working on identifying through comments the workflow steps.
 
-// Phase 1: in progress, nearly complete: Break workflow into composable steps without changing them much
-// *: cleanup options interface
-// *: extract workflow methods from main file
+// complete: Break workflow into composable steps without changing them much
+// complete: extract workflow methods from main file
+// started: cleanup options interface
 // *: cleanup workflow methods so they all present as over-rideable thennables
 // *: Update/add tests to unit test workflow steps independently
 // *: update docs and release
 
-var assert = require('assert');
 var ScopeContainer = require('./lib/scopeContainer');
-var decorateRequestWrapper = require('./lib/decorateRequestWrapper');
+var assert = require('assert');
 var buildProxyReq = require('./lib/buildProxyReq');
+var decorateRequestWrapper = require('./lib/decorateRequestWrapper');
+var decorateUserRes = require('./lib/decorateUserRes');
 var resolveProxyHost = require('./lib/resolveProxyHost');
 var sendProxyRequest = require('./lib/sendProxyRequest');
-var decorateUserRes = require('./lib/decorateUserRes');
+var sendUserRes = require('./lib/sendUserRes');
 
 module.exports = function proxy(host, userOptions) {
   assert(host, 'Host should not be empty');
@@ -31,25 +31,17 @@ module.exports = function proxy(host, userOptions) {
     // Do not proxy request if filter returns false.
     if (!Container.options.filter(req, res)) { return next(); }
 
+    // The Container object is passed down the chain of Promises, with each one
+    // of them mutating and returning Container.  The goal is to (eventually)
+    // present an interface of swappable promises; IOW, author using this library
+    // could replace/override any of these workflow steps
     buildProxyReq(Container)
-      //.then(determineProtocol)
       .then(resolveProxyHost)
       .then(decorateRequestWrapper) // the wrapper around request decorators.  this could use a better name
       .then(sendProxyRequest)
-      //.then(copyProxyResToUserRes)  // add this step by separaing decorateUserRes
       .then(decorateUserRes)
       .then(sendUserRes)
       .catch(next);
   };
 
 };
-
-function sendUserRes(Container) {
-    Promise.resolve(Container);
-    if (!Container.user.res.headersSent) {
-        Container.user.res.send(Container.proxy.resData);
-    }
-    Promise.resolve(Container);
-}
-
-
