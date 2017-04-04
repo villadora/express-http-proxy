ed49cc0df337ca66c050ddcd3b51c863ab9c4876
Nik Krimm <nkrimm@gmail.com>
2017-04-04 12:41:51 -0500

    extract resolveOptions to a module

--- index.js
+++ index.js
@@ -19,40 +19,11 @@ var https = require('https');
 var url = require('url');
 var zlib = require('zlib');
 var requestOptions = require('./lib/requestOptions');
-var isUnset = require('./lib/isUnset');
 var chunkLength = require('./lib/chunkLength');
 var Container = require('./lib/scopeContainer');
+var resolveOptions = require('./lib/resolveOptions');
 
 
-
-function resolveOptions(options) {
-  // resolve user argument to program usable options
-  // currenlty, we use a mix of strageies to allow an over-rider;
-  options = options || {};
-
-  if (options.decorateRequest) {
-    throw new Error(
-      'decorateRequest is REMOVED; use decorateReqOpt and decorateReqBody instead.  see README.md'
-    );
-  }
-
-  var  forwardPath = options.forwardPath || defaultForwardPath;
-
-  return {
-    decorateReqPath: options.forwardPathAsync || defaultForwardPathAsync(forwardPath),
-    filter: options.filter || defaultFilter,
-    decorateReqOpt: options.decorateReqOpt || function(reqOpt /*, req */) { return reqOpt; },
-    decorateReqBody: options.decorateReqBody || function(bodyContent /*, req*/) { return bodyContent; },
-    // For backwards compatability, we default to legacy behavior for newly added settings.
-    parseReqBody: isUnset(options.parseReqBody) ? true : options.parseReqBody,
-    reqBodyEncoding: options.reqBodyEncoding,
-    headers: options.headers,
-    preserveReqSession: options.preserveReqSession,
-    https: options.https,
-    port: options.port,
-    intercept: options.intercept
-  };
-}
 
 module.exports = function proxy(host, userOptions) {
   assert(host, 'Host should not be empty');
@@ -230,36 +201,12 @@ function parseHost(host, req, options) {
   };
 }
 
-function defaultFilter() {
-  // No-op version of filter.  Allows everything!
-
-  return true;
-}
-
-function defaultForwardPath(req) {
-
-  return url.parse(req.url).path;
-}
-
 function bodyEncoding(options) {
-
-
   /* For reqBodyEncoding, these is a meaningful difference between null and
    * undefined.  null should be passed forward as the value of reqBodyEncoding,
    * and undefined should result in utf-8.
    */
-
   return options.reqBodyEncoding !== undefined ? options.reqBodyEncoding: 'utf-8';
-}
-
-
-function defaultForwardPathAsync(forwardPath) {
-
-  return function(req, res) {
-    return new Promise(function(resolve) {
-      resolve(forwardPath(req, res));
-    });
-  };
 }
 
 function asBuffer(body, options) {
