ad94a58235b54b0e5ae96ec5a643fef07524bd81
monkpow <nkrimm@gmail.com>
2017-04-11 10:33:33 -0500

    Documentation improvements. (#213)

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 // complete: Break workflow into composable steps without changing them much
 // complete: extract workflow methods from main file
 // complete: cleanup options interface
+// *: change hook names to be different than the workflow steps.
 // *: cleanup host is processed twice
 // *: cleanup workflow methods so they all present as over-rideable thennables
 // *: Update/add tests to unit test workflow steps independently
