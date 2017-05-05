d14c8c5df2e3d431c52be4edec70647429c18756
jhonsonzhang <371054141@qq.com>
2017-05-06 00:08:23 +0800

    fix zlib error of \'Error: unexpected end of file\'

--- app/steps/decorateUserRes.js
+++ app/steps/decorateUserRes.js
@@ -10,7 +10,7 @@ function isResGzipped(res) {
 function zipOrUnzip(method) {
   return function(rspData, res) {
     return (isResGzipped(res)) ? zlib[method](rspData, {
-      finishFlush: zlib.Z_SYNC_FLUSH
+      finishFlush: zlib.constants.Z_SYNC_FLUSH
     }) : rspData;
   };
 }
