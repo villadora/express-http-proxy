90591815d1838da9ef53ff43fee395a6ac7e0e41
josh <postor@gmail.com>
2018-07-02 18:57:43 +0800

    case not exist

--- app/steps/sendProxyRequest.js
+++ app/steps/sendProxyRequest.js
@@ -50,7 +50,7 @@ function sendProxyRequest(Container) {
       if (bodyContent.length) {
         var body = bodyContent;
         var contentType = proxyReq.getHeader('Content-Type');
-        if (contentType.indexOf('x-www-form-urlencoded')>=0 || contentType.indexOf('application/x-www-form-urlencoded')>=0) {
+        if (contentType && (contentType.indexOf('x-www-form-urlencoded')>=0 || contentType.indexOf('application/x-www-form-urlencoded')>=0)) {
           try {
             var params = JSON.parse(body);
             body = Object.keys(params).map(function(k) { return k + '=' + params[k]; }).join('&');
