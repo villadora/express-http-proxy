0abefa01968cac591dd84c12718e99337de09197
siva.vadivel <siva.vadivel@imperva.com>
2018-08-31 01:03:52 -0700

    Fixed existing unit tests
    - Previously had a type of the new keyword

--- app/steps/continueToExpressAtEnd.js
+++ app/steps/continueToExpressAtEnd.js
@@ -1,16 +1,15 @@
 'use strict';
 
 function continueToExpressAtEnd(container) {
-    var resolverFn = container.options.skipToNextHandlerFilter || defaultSkipFilter;
 
-    return new Promise
-        .resolve()
-        .then(function() {
-            if (container.options.continueToExpressAtEnd) {
-                container.user.next();
-            }
-        })
-        .catch(Promise.reject);
+  return Promise
+      .resolve()
+    .then(function () {
+      if (container.options.continueToExpressAtEnd) {
+        container.user.next();
+      }
+    })
+    .catch(Promise.reject);
 }
 
 module.exports = continueToExpressAtEnd;
