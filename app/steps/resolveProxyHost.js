56c30b03676fa71f6bc01f3dc7d16e41dbabc9a8
maximilianstrauch <maxstrauch@googlemail.com>
2018-12-21 18:15:02 +0100

    Asynchronous "getHost" implementation. #284

--- app/steps/resolveProxyHost.js
+++ app/steps/resolveProxyHost.js
@@ -1,19 +1,19 @@
 'use strict';
 var requestOptions = require('../../lib/requestOptions');
 
-function resolveProxyHost(container) {
+async function resolveProxyHost(container) {
   var parsedHost;
 
   if (container.options.memoizeHost && container.options.memoizedHost) {
     parsedHost = container.options.memoizedHost;
   } else {
-    parsedHost = requestOptions.parseHost(container);
+    parsedHost = await requestOptions.parseHost(container);
   }
 
   container.proxy.reqBuilder.host = parsedHost.host;
   container.proxy.reqBuilder.port = container.options.port || parsedHost.port;
   container.proxy.requestModule = parsedHost.module;
-  return Promise.resolve(container);
+  return container;
 }
 
 module.exports = resolveProxyHost;
