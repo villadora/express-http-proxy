d4e63104469b289c479c1a4aee711b5988638e71
Connor White <connor.bcw@gmail.com>
2019-08-31 23:41:07 -0500

    Allow for async host functions

--- app/steps/resolveProxyHost.js
+++ app/steps/resolveProxyHost.js
@@ -1,13 +1,13 @@
 'use strict';
 var requestOptions = require('../../lib/requestOptions');
 
-function resolveProxyHost(container) {
+async function resolveProxyHost(container) {
   var parsedHost;
 
   if (container.options.memoizeHost && container.options.memoizedHost) {
     parsedHost = container.options.memoizedHost;
   } else {
-    parsedHost = requestOptions.parseHost(container);
+    parsedHost = await requestOptions.parseHost(container);
   }
 
   container.proxy.reqBuilder.host = parsedHost.host;
