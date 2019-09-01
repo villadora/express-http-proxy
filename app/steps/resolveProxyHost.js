d14466f38b6b33b2d281ac6ab3861c25e511b383
Connor White <connor.bcw@gmail.com>
2019-08-31 23:41:07 -0500

    Allow for async host functions.

--- app/steps/resolveProxyHost.js
+++ app/steps/resolveProxyHost.js
@@ -10,10 +10,12 @@ function resolveProxyHost(container) {
     parsedHost = requestOptions.parseHost(container);
   }
 
-  container.proxy.reqBuilder.host = parsedHost.host;
-  container.proxy.reqBuilder.port = container.options.port || parsedHost.port;
-  container.proxy.requestModule = parsedHost.module;
-  return Promise.resolve(container);
+  return Promise.resolve(parsedHost).then((parsedHost) => {
+    container.proxy.reqBuilder.host = parsedHost.host;
+    container.proxy.reqBuilder.port = container.options.port || parsedHost.port;
+    container.proxy.requestModule = parsedHost.module;
+    return Promise.resolve(container);
+  });
 }
 
 module.exports = resolveProxyHost;
