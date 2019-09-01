dc9013058cd38f115c760367e9820a3c075e11cb
Connor White <connor.bcw@gmail.com>
2019-09-01 10:31:02 -0500

    Node 6 support for async getHost

--- app/steps/resolveProxyHost.js
+++ app/steps/resolveProxyHost.js
@@ -1,19 +1,21 @@
 'use strict';
 var requestOptions = require('../../lib/requestOptions');
 
-async function resolveProxyHost(container) {
+function resolveProxyHost(container) {
   var parsedHost;
 
   if (container.options.memoizeHost && container.options.memoizedHost) {
     parsedHost = container.options.memoizedHost;
   } else {
-    parsedHost = await requestOptions.parseHost(container);
+    parsedHost = requestOptions.parseHost(container);
   }
 
-  container.proxy.reqBuilder.host = parsedHost.host;
-  container.proxy.reqBuilder.port = container.options.port || parsedHost.port;
-  container.proxy.requestModule = parsedHost.module;
-  return Promise.resolve(container);
+  return Promise.resolve(parsedHost).then((parsedHost) => {
+    container.proxy.reqBuilder.host = parsedHost.host;
+    container.proxy.reqBuilder.port = container.options.port || parsedHost.port;
+    container.proxy.requestModule = parsedHost.module;
+    return Promise.resolve(container);
+  });
 }
 
 module.exports = resolveProxyHost;
