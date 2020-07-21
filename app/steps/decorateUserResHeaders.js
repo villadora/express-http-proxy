2aec18894bfe4d06fc61d959cf718c9e1496226e
Nik 'Fire Eater' Krimm <nik@blue-lava.net>
2020-07-20 21:53:24 -0500

    [#453] Author should be able to delete headers in userResHeaderDecorator.

--- app/steps/decorateUserResHeaders.js
+++ app/steps/decorateUserResHeaders.js
@@ -9,10 +9,17 @@ function decorateUserResHeaders(container) {
     return Promise.resolve(container);
   }
 
+  const clearAllHeaders = (res) => {
+    for (const header in res._headers) {
+      res.removeHeader(header)
+    }
+  }
+
   return Promise
     .resolve(resolverFn(headers, container.user.req, container.user.res, container.proxy.req, container.proxy.res))
     .then(function(headers) {
       return new Promise(function(resolve) {
+        clearAllHeaders(container.user.res);
         container.user.res.set(headers);
         resolve(container);
       });
--- test/decorateUserResHeaders.js
+++ test/decorateUserResHeaders.js
@@ -19,12 +19,38 @@ describe('when userResHeaderDecorator is defined', function () {
   beforeEach(function () {
     app = express();
     var pTarget = express();
-    pTarget.use(function (req, res) { res.json(req.headers); });
+    pTarget.use(function (req, res) {
+      res.header('x-my-not-so-secret-header', 'minnie-mouse');
+      res.header('x-my-secret-header', 'mighty-mouse');
+      res.json(req.headers);
+    });
     serverReference = pTarget.listen(12345);
   });
 
   afterEach(function () {
     serverReference.close();
+  });
+
+  it('can delete a header', function (done) {
+
+    app.use('/proxy', proxy('http://127.0.0.1:12345', {
+      userResHeaderDecorator: function (headers /*, userReq, userRes, proxyReq, proxyRes */) {
+        delete headers['x-my-secret-header'];
+        return headers;
+      }
+    }));
+
+    app.use(function (req, res) {
+      res.sendStatus(200);
+    });
+
+    request(app)
+      .get('/proxy')
+      .expect(function (res) {
+        assert(Object.keys(res.headers).indexOf('x-my-not-so-secret-header') > -1);
+        assert(Object.keys(res.headers).indexOf('x-my-secret-header') === -1);
+      })
+      .end(done);
   });
 
   it('provides an interface for updating headers', function (done) {
@@ -47,4 +73,5 @@ describe('when userResHeaderDecorator is defined', function () {
       })
       .end(done);
   });
+
 });
