22ebc04b48a8f6a21fe271aea50db4d6dac40f84
Nik Krimm <nik.krimm@apollographql.com>
2025-05-03 01:02:02 -0500

    expand headers test

--- test/headers.js
+++ test/headers.js
@@ -50,3 +50,58 @@ describe('proxies headers', function () {
       });
   });
 });
+
+describe('Manipulating headers for the request to the proxy', function () {
+  this.timeout(TIMEOUT.QUICK);
+
+  var app;
+  var targetServer;
+  var TEST_PORT = 56001;
+  var PROXY_IP = '192.168.1.100';
+  var API_DOMAIN = 'localhost:' + TEST_PORT;
+
+  beforeEach(function () {
+    app = express();
+    targetServer = proxyTarget(TEST_PORT, 100, [{
+      method: 'get',
+      path: '/headers',
+      fn: function (req, res) {
+        assert.equal(req.headers['x-forwarded-for'], PROXY_IP);
+        assert.equal(req.headers['client-ip'], PROXY_IP);
+        assert.equal(req.headers['x-real-ip'], PROXY_IP);
+        res.json({ headers: req.headers });
+      }
+    }]);
+  });
+
+  afterEach(function () {
+    targetServer.close();
+  });
+
+  describe('e.g., IP forwarding headers', function () {
+    it('should pass user defined headers to the proxy target server', function (done) {
+      app.use(proxy(`http://${API_DOMAIN}`, {
+        proxyReqOptDecorator: function (proxyReqOpts) {
+          // Testing that the headers are present in the request to the proxy
+          proxyReqOpts.headers['X-Forwarded-For'] = PROXY_IP;
+          proxyReqOpts.headers['X-Real-IP'] = PROXY_IP;
+          proxyReqOpts.headers['Client-IP'] = PROXY_IP;
+          return proxyReqOpts;
+        }
+      }));
+
+      request(app)
+        .get('/headers')
+        .expect(200)
+        .end(function (err, res) {
+          if (err) { return done(err); }
+          // Verify again here to guard against failures to call `proxyReqOptDecorator`
+          assert.equal(res.body.headers['x-forwarded-for'], PROXY_IP);
+          assert.equal(res.body.headers['client-ip'], PROXY_IP);
+          assert.equal(res.body.headers['x-real-ip'], PROXY_IP);
+          done();
+        });
+    });
+  });
+});
+
